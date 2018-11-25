79d42850186ef2f6fb68d70e62d44b02e5207db5
Danilo Kordic <kordicdanilo@gmail.com>
2018-11-25 22:28:14 +0100

    added new lines 6

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration){
+    navigator.serviceWorker.register('/web-pwa/sw.js').then(function(registration){
       console.log(registration);
     }, function(err) {
       console.log(err);
